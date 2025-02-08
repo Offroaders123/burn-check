1b7874445174c822770a960994f805cd03b9dbd0
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-02-08 11:09:31 -0800

    JSDoc Migration
    This is a simple script, doesn't need to be in TS fully :)
    
    Wow I find myself writing JS like I was writing Rust now, aweseome, outstandingg!

--- index.js
+++ index.js
@@ -6,36 +6,43 @@ import { getAudioDurationInSeconds } from "get-audio-duration";
 import prettyBytes from "pretty-bytes";
 import prettyMilliseconds from "pretty-ms";
 
-const MAX_PLAYTIME: number = 4800000;
+/** @type {number} */
+const MAX_PLAYTIME = 4800000;
 
-const paths: string = argv.at(2) ?? (() => {
+/** @type {string} */
+const paths = argv.at(2) ?? (() => {
   console.error("Missing glob argument");
   exit(1);
 })();
 
-const files: string[] = await Array.fromAsync(glob(paths));
+/** @type {string[]} */
+const files = await Array.fromAsync(glob(paths));
 console.log(files.join("\n"), "\n");
 console.log(`${files.length} tracks`);
 
-const durations: number[] = await Promise.all(
+/** @type {number[]} */
+const durations = await Promise.all(
   files.map(async file =>
     await getAudioDurationInSeconds(file) * 1000
   )
 );
 
-const playtime: number = durations.reduce(
+/** @type {number} */
+const playtime = durations.reduce(
   (previous, current) => previous + current,
   0
 );
 console.log(`${prettyMilliseconds(playtime)} ${playtime < MAX_PLAYTIME ? "✅" : "❌"}`);
 
-const sizes: number[] = await Promise.all(
+/** @type {number[]} */
+const sizes = await Promise.all(
   files.map(async file =>
     (await stat(file)).size
   )
 );
 
-const size: number = sizes.reduce(
+/** @type {number} */
+const size = sizes.reduce(
   (previous, current) => previous + current,
   0
 );
